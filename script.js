5e0ba0ead4514cd3e8e4378ace2545bc34c531be
Garin Escorcia-Ariza <garinescorcia@yahoo.com>
2021-04-27 19:02:27 +0200

    create a loop

--- script.js
+++ script.js
@@ -7,10 +7,30 @@ const genRanNum = (num) => {
     return Math.floor(MAth.random() * num);
 }
 
+// stored data, for the personal data
 const collectiveData = {
     signInfo: ['Jupiter', 'Saturn', 'Mars', 'Neptune'],
-    fortuneOutout: ['bad luck for one year', '', '', ''],
-    advice: [],
-    jokes: []
+    fortuneOutout: ['a bad luck for one year', 'the luck of the gods', 'seven years of bad luck', 'luck for ten years'],
+    advice: ['do more exercise', 'do not eat too late', 'eat more vegetables', 'take at least five meals a day'],
+    popularSaying: ["if there was justice, there would be no brave", "do not say that the world sould be better, just do it", "shop here, don't be fooled elsewhere", "the works remain, the men go", "before enter, close the door"]
 }
 
+// store the selected information
+const personalData = [];
+
+// interate over the object
+for(let prop in collectiveData) {
+    let index = genRanNum(collectiveData[prop].length);
+
+    // use object's property to customize the message
+    switch(prop) {
+        case 'signInfo':
+            personalData.push(`Your sign right new is in ${collectiveData[prop][index]}`)
+            break
+        case 'fortuneOutput':
+            personalData.push(`You will have ${collectiveData[prop][index]}`)
+            break
+        case 'advice':
+            personalData.push
+    }
+};
