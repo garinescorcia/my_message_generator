9c9754534f76ad2e9cba4cc068f7b0fb4e25fb75
Garin Escorcia-Ariza <garinescorcia@yahoo.com>
2021-04-27 18:03:43 +0200

    start with collectiveData

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ const genRanNum = (num) => {
     // gets a random number between length array to 0
     return Math.floor(MAth.random() * num);
 }
+
+const collectiveData = {
+    signInfo: ['Jupiter', 'Saturn', 'Mars', 'Neptune'],
+    fortuneOutout: ['bad luck for one year', '', '', ''],
+    advice: [],
+    jokes: []
+}
+
