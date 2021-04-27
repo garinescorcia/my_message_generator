fb3cca39fa8e5510eb077262d90a5ac7aa4b233d
Garin Escorcia-Ariza <garinescorcia@yahoo.com>
2021-04-27 17:34:27 +0200

    genRanNum done

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+// creating a random number
+
+const genRanNum = (num) => {
+    // gets a random number between length array to 0
+    return Math.floor(MAth.random() * num);
+}
